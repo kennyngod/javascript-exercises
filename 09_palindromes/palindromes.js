cfe7d2d6e9f8a2fd9358b91f73bd513cb07aa3a4
Kenny Ngo <kngo@umich.edu>
2021-12-30 18:05:31 -0500

    pset09 completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
+const palindromes = function (string)
+{
+    string = string.toLowerCase();
+    const array = string.split('');
+    let left = 0;
+    let right = string.length - 1;
 
+    while (left < right)
+    {
+        while (!array[left].match(/[a-z]/i))
+            ++left;
+        while (!(array[right].match(/[a-z]/i)))
+            --right;
+        if (left >= right)
+            break;
+        if (array[left] != array[right])
+            return false;
+        ++left;
+        --right;
+    }
+    return true;
 };
 
 // Do not edit below this line
