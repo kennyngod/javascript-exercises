10f0abfc01fb64aa02ccbc7e1b26f542d38e3fb8
Kenny Ngo <kngo@umich.edu>
2021-12-30 16:19:25 -0500

    pset05 completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function (a, b)
+{
 
+    if (typeof(a) != "number" || typeof(b) != "number")
+        return "ERROR"
+    else if (a < 0 || b < 0)
+        return "ERROR";
+    
+    // swap a and b
+    if (a > b)
+        [a, b] = [b, a];
+    let sum = a;
+    do
+    {
+        sum += ++a;
+    } while (a != b);
+    return sum;
 };
 
 // Do not edit below this line
