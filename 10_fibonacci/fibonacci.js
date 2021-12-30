0d88adf2f65c543f208e6865110eec455b3181ef
Kenny Ngo <kngo@umich.edu>
2021-12-30 18:13:55 -0500

    pset10 completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (n)
+{
+    if (n < 0)
+        return "OOPS";
 
+    if (n == 0 || n == 1)
+        return 1;
+    n -= 2;
+    let f1 = 1;
+    let f2 = 1;
+    while (n--!= 0)
+    {
+        let temp = f2;
+        f2 += f1;
+        f1 = temp;
+    }
+    return f2;
 };
 
 // Do not edit below this line
