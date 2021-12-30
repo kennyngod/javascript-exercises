97ceda8b74bd4e81ebdf66d21ffddaccd0e356ae
Kenny Ngo <kngo@umich.edu>
2021-12-30 16:08:31 -0500

    pset04 completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args)
+{
+    array = args[0];
+    for (let i = 1; i < args.length; ++i)
+    {
+        const index = array.indexOf(args[i]);
+        if (index > -1)
+            array.splice(index, 1);
+    }
+    return array;
 };
 
 // Do not edit below this line
