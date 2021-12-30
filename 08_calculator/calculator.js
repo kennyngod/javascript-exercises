df882456e8fc99a75f30c02c07cf464705f4664b
Kenny Ngo <kngo@umich.edu>
2021-12-30 16:50:55 -0500

    pset08 completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,52 @@
-const add = function() {
-	
+const add = function (a, b)
+{
+    return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b)
+{
+    return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array)
+{
+    if (array.length == 0)
+        return 0;
+    let sum = 0;
+    for (let i = 0; i < array.length; ++i)
+        sum += array[i];
+    return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (array)
+{
+    let prod = 1;
+    for (let i = 0; i < array.length; ++i)
+        prod *= array[i];
+    return prod;
 };
 
-const power = function() {
-	
+const power = function (a, b)
+{
+    return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function (a)
+{
+    if (a <= 1)
+        return 1;
+    let prod = 1;
+    while (a != 1)
+        prod *= a--;
+    return prod;
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial
 };
