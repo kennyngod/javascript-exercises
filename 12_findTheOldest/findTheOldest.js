830415910e2e706af2626a17d6ef609876948cf8
Kenny Ngo <kngo@umich.edu>
2021-12-30 21:28:24 -0500

    pset12 completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function (people)
+{
+    let maxidx = 0;
+    let maxAge = 0;
+    for (let i = 0; i < people.length; ++i)
+    {
+        let person = people[i];
+        let stats = Object.values(person);
+        if (stats.length == 2)
+        {
+            day = new Date();
+            stats.push(day.getFullYear());
+        }
 
+        let age = stats[2] - stats[1];
+        if (age > maxAge)
+        {
+            maxidx = i;
+            maxAge = age;
+        }
+    }
+
+    return people[maxidx];
 };
 
 // Do not edit below this line
